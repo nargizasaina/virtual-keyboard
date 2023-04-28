eed8a3a23d07aa95d812ab39ab82b32b2b859310
Nargiza Sainazarova <nargizasaina@gmail.com>
2023-04-28 14:31:39 +0600

    feat: add capslock and bakcspace feature

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ let keyboard = [
     ['`', 'ё', 'backquote'], ['1', '1', 'one'], ['2', '2', 'two'], ['3', '3', 'three'], ['4', '4', 'four'], 
     ['5', '5', 'five'], ['6', '6', 'six'], ['7', '7', 'seven'], ['8', '8', 'eight'], ['9', '9', 'nine'], 
     ['0', '0', 'zero'], ['-', '-', 'minus'], ['=', '=', 'equal'], ['backspace', 'backspace', 'backspace'],
-    ['Tab', 'Tab', 'tab'], ['q', 'й', 'keyQ'], ['w', 'ц', 'keyW'], ['e', 'у', 'keyE'], ['r', 'к', 'keyR'], 
+    ['tab', 'tab', 'tab'], ['q', 'й', 'keyQ'], ['w', 'ц', 'keyW'], ['e', 'у', 'keyE'], ['r', 'к', 'keyR'], 
     ['t', 'е', 'keyT'], ['y', 'н', 'keyY'], ['u', 'г', 'keyU'], ['i', 'ш', 'keyI'], ['o', 'щ', 'keyO'], 
     ['p', 'з', 'keyP'], ['[', 'х', 'bracketOpen'], [']', 'ъ', 'bracketClose'], ['\\', '|', 'backslash'],
     ['caps lock', 'caps lock', 'capsLock'], ['a', 'ф', 'keyA'], ['s', 'ы', 'keyS'], ['d', 'в', 'keyD'], 
     ['f', 'а', 'keyF'], ['g', 'п', 'keyG'], ['h', 'р', 'keyH'], ['j', 'о', 'keyJ'], ['k', 'л', 'keyK'], 
-    ['l', 'д', 'keyL'], [';', 'ж', 'semicolon'], ["'", 'э', 'quote'], ['Enter', 'Enter', 'enter'],
+    ['l', 'д', 'keyL'], [';', 'ж', 'semicolon'], ["'", 'э', 'quote'], ['enter', 'enter', 'enter'],
     ['shift', 'shift', 'shiftLeft'], ['z', 'я', 'keyZ'], ['x', 'ч', 'keyX'], ['c', 'с', 'keyC'], 
     ['v', 'и', 'keyV'], ['b', 'и', 'keyB'], ['n', 'т', 'keyN'], ['m', 'ь', 'keyM'], [',', 'б', 'comma'], 
     ['.', 'ю', 'dot'], ['/', '.', 'slash'], ['shift', 'shift', 'shiftRight'],
@@ -67,13 +67,17 @@ let capsLock = false,
 
 const toggleCapsLock = () => {
   capsLock = !capsLock;
+
+  document.querySelectorAll('.letters').forEach((key) => {
+    capsLock ? key.innerHTML = key.innerHTML.toUpperCase() : key.innerHTML = key.innerHTML.toLocaleLowerCase();
+  });
 };
 
 const addKey = () => {
   keyboard.map(item => {
     const key = document.createElement('div');
     key.className = "key";
-    const nextLine = ['backspace', '\\', 'enter', '/'].indexOf(item[0]) !== -1;
+    let nextLine = ['backspace', 'backslash', 'enter', 'shiftRight'].indexOf(item[2]) !== -1;
 
     switch (item[0]) {
       case 'backspace':
@@ -81,7 +85,8 @@ const addKey = () => {
         key.innerHTML = item[0];
 
         key.addEventListener('click', () => {
-          textArea.value = textArea.value.slice(0, textArea.length - 1);
+          let text = textArea.value;
+          textArea.value = text.slice(0, text.length - 1);
         });
         break;
 
@@ -94,13 +99,14 @@ const addKey = () => {
         }); 
         break;
 
-      case 'capsLock':
+      case 'caps lock':
         key.className = "key wide-btn";
         key.innerHTML = item[0];
 
         key.addEventListener('click', () => {
             toggleCapsLock();
         });
+        console.log(capsLock);
         break;
 
       case 'enter':
@@ -137,13 +143,15 @@ const addKey = () => {
         break;
 
       default: {
-        let [, letter] = item;
-        if (!lang === 'en') {
+        key.classList.add('letters');
+        let [letter] = item;
+        if (lang !== 'en') {
           [letter] = item;
         }
         key.innerHTML = letter;
 
         key.addEventListener('click', () => {
+          // console.log(capsLock);
           if (capsLock) {
             textArea.value += letter.toUpperCase();
           } else {
@@ -154,7 +162,11 @@ const addKey = () => {
       }
     }
     board.append(key);
+    if (nextLine) {
+        board.appendChild(document.createElement('br'));
+    }
   });
+  
 };
 
 addKey();
